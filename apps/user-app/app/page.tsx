549d0b8f36399ca7ef5e9155681b24f1de2f17d9
Gagan Bhardwaj <gaganbhardwaj514@gmail.com>
2024-11-27 00:54:12 +0530

    feat(user-app): integrate Prisma for database management and add initial migration

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
